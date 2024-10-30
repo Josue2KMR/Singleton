0edae36d1eefb9a6e5bef76fc65e5274abb2c6df
Josue2kMR <josuernesto05@gmail.com>
2024-10-30 10:17:27 -0300

    creacion de la clase singleton

---
+++ src/Library/Singleton.cs
@@ -0,0 +1,14 @@
+﻿namespace Library;
+
+public abstract class Singleton<T> where T : new()
+{
+
+    private static T? _instance = new();
+    
+    private Singleton() { }
+        
+    public static T Instance
+    {
+        return _instance.value;
+    }
+}
